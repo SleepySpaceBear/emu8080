a239e50d1e4ae84d336958dd4a9573f35c715166
Bear <sleepyspacedad@gmail.com>
2023-12-29 15:14:59 -0500

    Added parity unit test and fixed parity_even impl

--- src/utils.rs
+++ src/utils.rs
@@ -13,9 +13,9 @@ pub fn check_aux_carry(old_val: u8, new_val: u8) -> bool {
 }
 
 pub fn parity_even(val: u8) -> bool {
-    let val: u8 = val ^ (val >> 4);
-    let val: u8 = val ^ (val >> 2);
-    let val: u8 = val ^ (val >> 1);
+    let val: u8 = (0x0F & val) ^ (val >> 4);
+    let val: u8 = (0x03 & val) ^ (val >> 2);
+    let val: u8 = (0x01 & val) ^ (val >> 1);
     return val == 0;
 }
 
@@ -25,4 +25,18 @@ pub fn twos_complement(val: u8) -> u8 {
 
 pub fn make_u16(higher_order: u8, lower_order: u8) -> u16 {
     return ((higher_order as u16) << 8) | (lower_order as u16)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parity() {
+        assert_eq!(true, parity_even(0x0));
+        assert_eq!(false, parity_even(0x1));
+        assert_eq!(true, parity_even(0b11001100));
+        assert_eq!(true, parity_even(0b11001001));
+        assert_eq!(false, parity_even(0b10001100));
+    }
 }
