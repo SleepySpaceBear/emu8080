bc057760ae5f039d0bcf7ebce9b3e3b6815576f4
Bear <sleepyspacedad@gmail.com>
2023-12-29 19:57:00 -0500

    Added unit tests

--- src/registers.rs
+++ src/registers.rs
@@ -144,8 +144,8 @@ impl Registers {
     }
 
     pub fn set_psw(&mut self, val: u16) {
-       self.accumulator = ((val >> 8) & 0xFF) as u8;
-       self.status = (val & 0xFF) as u8
+        self.accumulator = ((val >> 8) & 0xFF) as u8;
+        self.set_status((val & 0xFF) as u8);
     }
 
     pub fn accumulator(&self) -> u8 {
@@ -247,5 +247,141 @@ impl Registers {
 
     pub fn pair_w(&self) -> u16 {
        make_u16(self.w(), self.z())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_reg_pair_b() {
+        let mut regs = Registers::new();
+
+        regs.set_b(0x5);
+        assert_eq!(regs.b(), 0x5);
+
+        regs.set_c(0x7);
+        assert_eq!(regs.c(), 0x7);
+
+        assert_eq!(regs.pair_b(), 0x0507);
+
+        regs.set_pair_b(0x0705);
+        assert_eq!(regs.b(), 0x7);
+        assert_eq!(regs.c(), 0x5);
+    }
+
+    #[test]
+    fn test_reg_pair_d() {
+        let mut regs = Registers::new();
+        
+        regs.set_d(0x5);
+        assert_eq!(regs.d(), 0x5);
+
+        regs.set_e(0x7);
+        assert_eq!(regs.e(), 0x7);
+
+        assert_eq!(regs.pair_d(), 0x0507);
+        
+        regs.set_pair_d(0x0705);
+        assert_eq!(regs.d(), 0x7);
+        assert_eq!(regs.e(), 0x5);
+    }
+   
+    #[test]
+    fn test_reg_pair_h() {
+        let mut regs = Registers::new();
+        
+        regs.set_h(0x5);
+        assert_eq!(regs.h(), 0x5);
+
+        regs.set_l(0x7);
+        assert_eq!(regs.l(), 0x7);
+
+        assert_eq!(regs.pair_h(), 0x0507);
+
+        regs.set_pair_h(0x0705);
+        assert_eq!(regs.h(), 0x7);
+        assert_eq!(regs.l(), 0x5);
+    }
+
+    #[test]
+    fn test_reg_pair_w() {
+        let mut regs = Registers::new();
+        
+        regs.set_w(0x5);
+        assert_eq!(regs.w(), 0x5);
+
+        regs.set_z(0x7);
+        assert_eq!(regs.z(), 0x7);
+
+        assert_eq!(regs.pair_w(), 0x0507);
+    }
+
+    #[test]
+    fn test_status() {
+        let mut regs = Registers::new();
+
+        // verify that certain bits in the PSW can't have invalid values
+        regs.set_psw(0xFFFF);
+        assert_eq!(regs.accumulator(), 0xFF);
+        assert_eq!(regs.status(), 0xC7);
+        
+        regs.set_psw(0x0000);
+        assert_eq!(regs.accumulator(), 0x00);
+        assert_eq!(regs.status(), 0x02);
+        
+        regs.set_status(0xFF);
+        assert_eq!(regs.status(), 0xC7);
+
+        regs.set_status(0x00);
+        assert_eq!(regs.status(), 0x02);
+
+
+        regs.set_status_sign(true);
+        assert_ne!(regs.status() & (StatusFlags::SignBit as u8), 0);
+        assert!(regs.status_sign());
+
+        regs.set_status_sign(false);
+        assert_eq!(regs.status() & (StatusFlags::SignBit as u8), 0);
+        assert!(!regs.status_sign());
+
+
+        regs.set_status_zero(true);
+        assert_ne!(regs.status() & (StatusFlags::ZeroBit as u8), 0);
+        assert!(regs.status_zero());
+
+        regs.set_status_zero(false);
+        assert_eq!(regs.status() & (StatusFlags::ZeroBit as u8), 0);
+        assert!(!regs.status_zero());
+
+
+        regs.set_status_aux_carry(true);
+        assert_ne!(regs.status() & (StatusFlags::AuxCarryBit as u8), 0);
+        assert!(regs.status_aux_carry());
+
+        regs.set_status_aux_carry(false);
+        assert_eq!(regs.status() & (StatusFlags::AuxCarryBit as u8), 0);
+        assert!(!regs.status_aux_carry());
+
+
+        regs.set_status_parity(true);
+        assert_ne!(regs.status() & (StatusFlags::ParityBit as u8), 0);
+        assert!(regs.status_parity());
+
+        regs.set_status_parity(false);
+        assert_eq!(regs.status() & (StatusFlags::ParityBit as u8), 0);
+        assert!(!regs.status_parity());
+
+
+        regs.set_status_carry(true);
+        assert_ne!(regs.status() & (StatusFlags::CarryBit as u8), 0);
+        assert!(regs.status_carry());
+
+        regs.set_status_carry(false);
+        assert_eq!(regs.status() & (StatusFlags::CarryBit as u8), 0);
+        assert!(!regs.status_carry());
+
+        assert_eq!(regs.status(), 0x2);
     }
 }
