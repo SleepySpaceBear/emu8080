d750c3ba2c21d1c2634500c215405441cd516110
Bear <sleepyspacedad@gmail.com>
2024-01-08 22:04:07 -0500

    Added MemoryAccess trait

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,15 @@ mod registers;
 mod cpu;
 mod utils;
 
+use std::io::{stdin,Read};
+
 fn main() {
     let mut memory: [u8; 200] = [0;200];
 
     let mut cpu = cpu::Intel8080::new(&mut memory);
+
+    let mut byte = [0u8];
+    while let Ok(_) = stdin().read(&mut byte) {
+        println!("CHAR {:?}", byte[0] as char);
+    }
 }
---
+++ src/memory.rs
@@ -0,0 +1,4 @@
+pub trait MemoryAccess {
+    fn read(self, addr: u16) -> u8;
+    fn write(self ,addr: u16, val: u8);
+}
