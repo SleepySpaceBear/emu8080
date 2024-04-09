e5331636bea81c72cd835aee6b93b68ea5836c6d
Bear <sleepyspacedad@gmail.com>
2024-04-09 19:43:07 -0400

    Defined public interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-mod cpu;
-mod memory;
+pub mod cpu;
+pub mod memory;
 mod registers;
 mod utils;
+
+pub use cpu::Instruction;
+pub use cpu::Intel8080;
+pub use memory::MemoryAccess;
+pub use memory::Memory;
+
