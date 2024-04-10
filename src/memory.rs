199aa8cbfec60873fd1c8d5b7896947b67d83dd3
Bear <sleepyspacedad@gmail.com>
2024-04-09 20:04:01 -0400

    Moved bulk read/writes from MemoryAccess trait to Memory struct

--- src/memory.rs
+++ src/memory.rs
@@ -1,8 +1,6 @@
 pub trait MemoryAccess {
     fn read_byte(&self, addr: u16) -> u8;
     fn write_byte(&mut self, addr: u16, val: u8);
-    fn copy_into_from_slice(&mut self, src: &[u8], index: u16);
-    fn get_bytes(&self, start: u16, end: u16) -> &[u8];
 }
 
 pub struct Memory<const N: usize> {
@@ -14,6 +12,15 @@ impl<const N: usize> Memory<N> {
         return Memory {
             arr: [0 as u8; N]
         }
+    }
+
+    pub fn copy_into_from_slice(&mut self, src: &[u8], index: u16) {
+        let end = std::cmp::min(index as usize + src.len(), N);
+        self.arr[index as usize..end].copy_from_slice(src);
+    }
+
+    pub fn get_bytes(&self, start: u16, end: u16) -> &[u8] {
+        &self.arr[start as usize..end as usize]
     }
 }
 
@@ -32,13 +39,5 @@ impl<const N: usize> MemoryAccess for Memory<N> {
         }
     }
 
-    fn copy_into_from_slice(&mut self, src: &[u8], index: u16) {
-        let end = std::cmp::min(index as usize + src.len(), N);
-        self.arr[index as usize..end].copy_from_slice(src);
-    }
-
-    fn get_bytes(&self, start: u16, end: u16) -> &[u8] {
-        &self.arr[start as usize..end as usize]
-    }
 }
 
